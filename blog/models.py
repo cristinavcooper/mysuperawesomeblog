62e2b9d9c2fe9aa10bc6f01c05826625f81e273e
Медведева Кристина Игоревна <medvedeva.k@tps.local>
2021-09-21 20:08:29 +0300

    Second

--- blog/models.py
+++ blog/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 class Post(models.Model):
-	title = models.CharField(max_lenght=300)
+	title = models.CharField(max_length=300)
 	date = models.DateTimeField()
-	text = models.CharField()
+	text = models.CharField(max_length=100)
 	image = models.ImageField(upload_to='event_images/') 
--- myawesomeblog/settings.py
+++ myawesomeblog/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'myawesomeblog.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'mysuperawesomeblog',
+        'USER': 'postgres',
+        'PASSWORD': '1234567',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
