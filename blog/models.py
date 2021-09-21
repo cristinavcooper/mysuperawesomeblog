a9a1d3be9d77e0b8dc8836831ba6ef59b8264a77
Медведева Кристина Игоревна <medvedeva.k@tps.local>
2021-09-21 20:24:14 +0300

    Add post model

--- blog/models.py
+++ blog/models.py
@@ -4,5 +4,5 @@ from django.db import models
 class Post(models.Model):
 	title = models.CharField(max_length=300)
 	date = models.DateTimeField()
-	text = models.CharField(max_length=100)
+	text = models.TextField()
 	image = models.ImageField(upload_to='event_images/') 
